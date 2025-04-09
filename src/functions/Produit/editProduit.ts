160d7f3cdbecd2f0a14d880d382f844f93556634
abbv75 <bore.younous59@gmail.com>
2025-04-09 14:35:08 +0000

    add and edit product

---
+++ src/functions/Produit/editProduit.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { PRODUIT_T } from "../../types";
+
+//@ts-ignore
+const Api_Url = import.meta.env.VITE_API_URL;
+
+export const editProduit = async (
+    id: string,
+    product_name: string,
+    description: string,
+    price: number,
+    establishment: string,
+    category: string,
+) => {
+    try {
+        const { data }: { data: PRODUIT_T } = await axios.post(
+            `${Api_Url}products/${id}/`,
+            {
+                product_name,
+                description,
+                price,
+                establishment,
+                category,
+            }
+        );
+        return data;
+    } catch (error) {
+        console.error("Erreur Lors de la modification de la boutique:", error);
+        return false;
+    }
+}
