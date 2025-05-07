2ca5587f8a936705b90824a20449e4bda8e1ed76
Mohamed <mohamedmoustaphatr@gmail.com>
2025-05-07 17:31:27 +0000

    Secure route

--- src/functions/Commandes/Commandes.jsx
+++ src/functions/Commandes/Commandes.jsx
@@ -2,7 +2,7 @@ import axiosClient from "../../axiosClient";
 // Recuperation de la liste des   orders
 export const GetAllCommandes = async () => {
   try {
-    const response = await axiosClient.get(`orders/`);
+    const response = await axiosClient.get(`orders`);
     return response.data;
   } catch (error) {
     console.error('Error fetching   orders:', error);
